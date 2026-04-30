d1ac446f927f8081df680257a25af75749e74e38
Nattsu39 <nattsu39@outlook.com>
2026-04-30 21:50:43 +0800

    feat(help_plugin): 重写帮助插件

---
+++ ironsbot/plugins/help/config.py
@@ -0,0 +1,10 @@
+from nonebot import get_plugin_config
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    help_ignored_plugins: list[str] = []
+    """需要忽略的插件名称列表"""
+
+
+plugin_config = get_plugin_config(Config)
